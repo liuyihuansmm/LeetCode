484d704a192f0eb4ada53821a03485a9672c9e91
LYH <liuyihuansmm@163.com>
2017-01-04 14:44:16 +0800

    ReverseString

---
+++ src/_344ReverseString/Solution.java
@@ -0,0 +1,25 @@
+package _344ReverseString;
+
+/**
+ * Write a function that takes a string as input and returns the string reversed.
+ * @author LYH
+ *
+ */
+public class Solution {
+	
+	public String reverseString(String s) {
+        char[] strCharArray = s.toCharArray();
+        for(int i=0;i<strCharArray.length/2;i++){
+        	char temp = strCharArray[i];
+        	strCharArray[i] = strCharArray[strCharArray.length-1-i];
+        	strCharArray[strCharArray.length-1-i]=temp;
+        }
+        return String.valueOf(strCharArray);
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new Solution().reverseString("liuyihuans"));
+	}
+
+}
